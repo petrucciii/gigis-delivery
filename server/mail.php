bd33bc5aa19e715b7ccdab687fad0ea0388343ce
petrucciii <riccardo.petrucci99@gmail.com>
2021-08-16 12:21:12 +0200

    mail.php

--- server/mail.php
+++ server/mail.php
@@ -1,5 +1,17 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    header('Content-Type: application/json');
+
     function sendMail($reciver, $subject, $message, $headers) {
         mail($reciver, $subject, $message, $headers);
     }
+
+    $reciver = "riccardo.petrucci99@gmail.com";
+    $subject = "Rider";
+    $message = "motivo: " . $_POST['reason'] . ", esperienza: " . $_POST['experience'];
+    $headers = "email inviata da: " . $_POST['name'] . ", " . $_POST['email'] . ", residente a: " . $_POST['city'];
+
+
+    sendMail($reciver, $subject, $message, $headers);
 ?>
